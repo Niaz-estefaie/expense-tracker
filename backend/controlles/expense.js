087e3664f8f4dd8ea33724352a30ba5fe47bf522
Niaz <niaz.e@bugloos.com>
2024-07-30 17:39:26 +0330

    (feature): exense added to the project

---
+++ backend/controlles/expense.js
@@ -0,0 +1,46 @@
+const { getMessage } = require('../helpers/error-messages');
+const { ExpenseSchema } = require('../models/expense.model')
+
+exports.addExpense = async (req, res) => {
+    const { title, amount, category, description, date } = req.body;
+
+    const expense = ExpenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    });
+
+    try {
+        if (!title || !category || !description || !date) {
+            return res.status(400).json(getMessage(400))
+        }
+
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json(getMessage(400))
+        }
+        await expense.save();
+        res.status(200).json(getMessage(200))
+    } catch (error) {
+        res.status(500).json(getMessage(500))
+    }
+}
+
+exports.getExpenses = async (req, res) => {
+    try {
+        const expenses = await ExpenseSchema.find().sort({ createdAt: -1 });
+        res.status(200).json(getMessage(200))
+    } catch (error) {
+        res.status(500).json(getMessage(500))
+    }
+}
+
+exports.deleteExpense = async (req, res) => {
+    const { id } = req.params;
+    ExpenseSchema.findByIdAndDelete(id).then((expense) => {
+        res.status(200).json(getMessage(200))
+    }).catch((error) => {
+        res.status(500).json(getMessage(500))
+    })
+}
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,9 +1,13 @@
+const { addExpense, getExpenses, deleteExpense } = require('../controlles/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controlles/income');
 
 const router = require('express').Router();
 
 router.post('/income', addIncome)
 .get('/incomes', getIncomes)
-.delete('/income/:id', deleteIncome);
+.delete('/income/:id', deleteIncome)
+.post('expense', addExpense)
+.get('expenses', getExpenses)
+.delete('expense/:id', deleteExpense)
 
 module.exports = router;
